eb6fc9dab73c4c4052ca04eb8028173a934e76b1
Murilo Pereira <mrex@tuta.io>
2023-11-05 01:56:15 +0100

    init: First Commit

---
+++ src/renderer/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Header from './components/Header.vue'
+</script>
+
+<template>
+  <Header />
+</template>
+
+<style lang="less">
+@import './assets/css/styles.less';
+</style>
---
+++ src/renderer/src/components/Header.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <nav class="flex items-center justify-between w-full">
+    <h1 class="text-4xl">Crypto Server</h1>
+    <div class="flex">
+      <p class="mr-5 font-bold">Server: <span class="font-normal">Stopped</span></p>
+      <p class="font-bold">Password: <span class="font-normal">Unset</span></p>
+    </div>
+  </nav>
+</template>
+
+<style scoped lang="less"></style>
